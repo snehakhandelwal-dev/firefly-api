4ad540e9ff8e61d65c2b15bdcb81b46c1f32bc55
snehakhandelwal-dev <sneha.developer29@gmail.com>
2025-05-09 15:04:08 +0530

    Replace bcrypt with bcryptjs for compatibility

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -1,6 +1,6 @@
 const UsersModel = require("../models/userSchema");
 const jwt = require("jsonwebtoken");
-const bcrypt = require("bcrypt");
+const bcrypt = require('bcryptjs');
 const crypto = require("crypto");
 require("dotenv").config();
 exports.register = async (req, res) => {
